0c7f4815f76af6984953527ab30469c3fd91714f
PoProstuWitold <witekzawada@o2.pl>
2023-02-10 19:39:50 +0100

    favicon

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -13,6 +13,10 @@ export const SEO: React.FC<SeoProps> = ({ description, title }) => {
 	return (
 		<Head>
             <title>{`${title}`}</title>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
             <meta name="description" content={description ? description : config.description} />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
