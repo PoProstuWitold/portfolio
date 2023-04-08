6f3498fa942b671d3c63c74d974aae1cb33d6e48
PoProstuWitold <witekzawada@o2.pl>
2023-04-08 20:39:48 +0200

    Contact form UI tweaks

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -7,8 +7,8 @@ const Contact: React.FC = () => {
 	return (
 		<section id="contact" className="min-h-screen pt-20 flex flex-col gap-8 py-10 cursor-default bg-base-100">
 			<h1 className="text-4xl font-bold border-b-[5px] w-fit mx-auto pb-2 border-primary">Contact me</h1>
-			<div className="flex flex-col lg:flex-row lg:mx-14">
-			<div className="m-5 text-left lg:w-1/3">
+			<div className="lg:mx-[5rem] flex flex-col lg:flex-row mx-6 gap-6">
+			<div className="text-left lg:w-1/3">
 				<h2 className="mb-4 text-4xl font-semibold">Get in touch</h2>
 				<p className="text-2xl text-justify">
 					If you have any questions feel free to contact me using informations below or contact form
@@ -43,15 +43,15 @@ const Contact: React.FC = () => {
 				</div>
 			</div>
 			<div
-				className="m-5 lg:w-2/3 rounded-2xl bg-base-200 border-primary"
+				className="lg:w-2/3 rounded-2xl bg-base-200 border-primary"
 				style={{ borderColor: 'hsl(var(--n)' }}
 			>
 				<div className="p-6 rounded-2xl shadow-md">
-					<form>
-						<div className="flex flex-col justify-between mx-auto lg:flex-row">
-							<div className="w-full mb-5 mr-10 form-control">
+					<form className='flex flex-col gap-6'>
+						<div className="flex flex-col justify-between gap-6 lg:flex-row">
+							<div className="w-full form-control">
 								<label className="label">
-									<span className="label-text">Name</span>
+									<span className="label-text font-semibold">Name</span>
 									<span className="italic label-text-alt">
 										e.g. Witold
 									</span>
@@ -62,9 +62,9 @@ const Contact: React.FC = () => {
 									className="w-full input input-bordered"
 								/>
 							</div>
-							<div className="w-full mb-5 form-control">
+							<div className="w-full form-control">
 								<label className="label">
-									<span className="label-text">Email</span>
+									<span className="label-text font-semibold">Email</span>
 									<span className="italic label-text-alt">
 										e.g. witold@email.com
 									</span>
@@ -76,9 +76,9 @@ const Contact: React.FC = () => {
 								/>
 							</div>
 						</div>
-						<div className="mb-5 form-control">
+						<div className="form-control">
 							<label className="label">
-								<span className="label-text">Title</span>
+								<span className="label-text font-semibold">Title</span>
 								<span className="italic label-text-alt">
 									e.g. New amazing project..
 								</span>
@@ -89,22 +89,22 @@ const Contact: React.FC = () => {
 									className="w-full input input-bordered"
 								/>
 						</div>
-						<div className="mb-10 form-control">
+						<div className="form-control mb-6">
 							<label className="label">
-								<span className="label-text">Content</span>
+								<span className="label-text font-semibold">Content</span>
 								<span className="italic label-text-alt">
 									e.g. Hello Witold, I'm writing to ask...
 								</span>
 							</label>
 							<textarea
-								className="h-24 textarea textarea-bordered"
+								className="textarea textarea-md textarea-bordered"
 								placeholder="Content of your message"
 							></textarea>
 						</div>
 						<div className="flex justify-center">
 							<button
 								type="submit"
-								className="btn btn-lg btn-outline w-60"
+								className="btn btn-lg btn-outline md:w-full"
 								disabled
 							>
 								Work in progress
