3b4374b09477ece1790d7126b7ba60b2ac1218fb
PoProstuWitold <witekzawada@o2.pl>
2025-01-25 17:28:50 +0100

    fix: tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,14 +1,9 @@
-import { themes, mySkills, projects } from '../src/utils/constans'
+import { themes, projects } from '../src/utils/constans'
 
 describe('Constans length tests', () => {
 	describe('Themes', () => {
 		it('should return array of length 7', async () => {
 			expect(themes).toHaveLength(7)
-		})
-	})
-	describe('mySkills', () => {
-		it('should be defined', async () => {
-			expect(mySkills).toBeDefined()
 		})
 	})
 	describe('projects', () => {
